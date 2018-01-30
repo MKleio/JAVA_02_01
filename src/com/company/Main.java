65b16ad064ee3f82cafcf8070882b516a7669b5c
tomassimkus <to>
2018-01-30 14:33:37 +0200

    Second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Iveskite ugi");
+        Scanner sc = new Scanner(System.in);
+        double ugis = sc.nextFloat();
+        System.out.println("Iveskite svori");
+        double svoris = sc.nextFloat();
+        System.out.println("KMI: " + skaiciuokKMI(svoris,ugis));
+    }
+
+    public static double skaiciuokKMI(double svoris, double ugis) {
+        return svoris / Math.pow(ugis,2);
     }
 }
